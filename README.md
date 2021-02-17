a9d5d4a92fc6b8bfc2066215b8fbabcc32141f41
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2021-02-17 14:06:14 +0100

    local planner package template (based on DWA) created

---
+++ README.md
@@ -0,0 +1,3 @@
+# hubero_local_planner
+
+A local planning module for A Framework to Simulate Human Behaviour in Robot Research
---
+++ src/hubero_local_planner_ros.cpp
@@ -0,0 +1,55 @@
+#include <hubero_local_planner/HuberoPlannerROS.h>
+#include <Eigen/Core>
+#include <cmath>
+
+#include <ros/console.h>
+
+#include <pluginlib/class_list_macros.h>
+
+#include <base_local_planner/goal_functions.h>
+#include <nav_msgs/Path.h>
+
+//register this planner as a BaseLocalPlanner plugin
+PLUGINLIB_EXPORT_CLASS(hubero_local_planner::HuberoPlannerROS, nav_core::BaseLocalPlanner)
+
+namespace hubero_local_planner {
+
+void HuberoPlannerROS::reconfigureCB(HuberoPlannerConfig &config, uint32_t level) {
+}
+
+HuberoPlannerROS::HuberoPlannerROS() : initialized_(false),
+	odom_helper_("odom"), setup_(false), dsrv_(nullptr) {
+}
+
+void HuberoPlannerROS::initialize(
+	std::string name,
+	tf::TransformListener* tf,
+	costmap_2d::Costmap2DROS* costmap_ros) {
+}
+
+bool HuberoPlannerROS::setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan) {
+	return true;
+}
+
+bool HuberoPlannerROS::isGoalReached() {
+	return true;
+}
+
+void HuberoPlannerROS::publishLocalPlan(std::vector<geometry_msgs::PoseStamped>& path) {
+	base_local_planner::publishPlan(path, l_plan_pub_);
+}
+
+void HuberoPlannerROS::publishGlobalPlan(std::vector<geometry_msgs::PoseStamped>& path) {
+	base_local_planner::publishPlan(path, g_plan_pub_);
+}
+
+HuberoPlannerROS::~HuberoPlannerROS(){
+	//make sure to clean things up
+	delete dsrv_;
+}
+
+bool HuberoPlannerROS::computeVelocityCommands(geometry_msgs::Twist& cmd_vel) {
+	return true;
+}
+
+}; // namespace hubero_local_planner
