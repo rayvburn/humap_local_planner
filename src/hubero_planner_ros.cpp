b4476169db2a044765bda8a3e3e6cac388f23664
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2021-02-26 16:08:31 +0100

    HuberoPlanner and ROS code cleaned up, only used instances left

--- src/hubero_planner_ros.cpp
+++ src/hubero_planner_ros.cpp
@@ -1,3 +1,4 @@
+#include <hubero_local_planner/hubero_planner_ros.h>
 #include <Eigen/Core>
 #include <cmath>
 
@@ -6,7 +7,6 @@
 #include <pluginlib/class_list_macros.h>
 
 #include <base_local_planner/goal_functions.h>
-#include <hubero_local_planner/hubero_planner_ros.h>
 #include <nav_msgs/Path.h>
 
 //register this planner as a BaseLocalPlanner plugin
@@ -15,12 +15,15 @@ PLUGINLIB_EXPORT_CLASS(hubero_local_planner::HuberoPlannerROS, nav_core::BaseLoc
 namespace hubero_local_planner {
 
 HuberoPlannerROS::HuberoPlannerROS():
-		costmap_ros_(NULL), tf_(NULL), /*costmap_model_(NULL),*/
-		costmap_converter_loader_("costmap_converter", "costmap_converter::BaseCostmapToPolygons"),
 		initialized_(false),
-		odom_helper_("odom"),
-		setup_(false),
-		dsrv_(nullptr) {
+		planner_util_(nullptr),
+		planner_(nullptr),
+		costmap_ros_(nullptr),
+		costmap_converter_loader_("costmap_converter", "costmap_converter::BaseCostmapToPolygons"),
+		dsrv_(nullptr),
+		cfg_(nullptr),
+		tf_(nullptr),
+		odom_helper_("odom") {
 }
 
 HuberoPlannerROS::~HuberoPlannerROS(){
@@ -28,23 +31,50 @@ HuberoPlannerROS::~HuberoPlannerROS(){
 	delete dsrv_;
 }
 
-void HuberoPlannerROS::initialize(
-		std::string name,
-		tf::TransformListener* tf,
-		costmap_2d::Costmap2DROS* costmap_ros) {
-	// check if the plugin is already initialized
-	if (!initialized_) {
-		// create Node Handle with name of plugin (as used in move_base for loading)
-		ros::NodeHandle nh("~/" + name);
+void HuberoPlannerROS::initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros) {
+	printf("[HuberoPlannerROS::initialize] start\r\n");
 
-		// get parameters of TebConfig via the nodehandle and override the default config
-		cfg_.loadFromParamServer(nh);
+	// check if the plugin is already initialized
+	if (!isInitialized()) {
+		// create Node Handle with name of plugin (as used in move_base for loading)
+		ros::NodeHandle private_nh("~/" + name);
+
+		// assign args
+		tf_ = tf;
+		costmap_ros_ = costmap_ros;
+		costmap_ros_->getRobotPose(current_pose_);
+
+		costmap_2d::Costmap2D* costmap = costmap_ros_->getCostmap();
+
+		planner_util_ = std::make_shared<base_local_planner::LocalPlannerUtil>();
+		planner_util_->initialize(tf, costmap, costmap_ros_->getGlobalFrameID());
 
 		// reserve some memory for obstacles
 		obstacles_.reserve(500);
 
-		// ...
+		// get parameters of HuberoConfig via the node handle and override the default config
+		cfg_ = std::make_shared<HuberoConfigROS>();
+		cfg_->loadFromParamServer(private_nh);
+
+		// local planner
+		printf("[HuberoPlannerROS::initialize] before planner\r\n");
+		planner_ = std::make_shared<HuberoPlanner>(name, planner_util_);
+		planner_->initialize(cfg_);
+
+		// dynamic reconfigure server
+		dsrv_ = new dynamic_reconfigure::Server<HuberoPlannerConfig>(private_nh);
+		dynamic_reconfigure::Server<HuberoPlannerConfig>::CallbackType cb =
+			boost::bind(&HuberoPlannerROS::reconfigureCB, this, _1, _2);
+		dsrv_->setCallback(cb);
+
+	    // set initialized flag
+	    initialized_ = true;
+
+	    ROS_DEBUG("hubero_local_planner plugin initialized.");
+	    return;
 	}
+
+	ROS_DEBUG("hubero_local_planner plugin has already been initialized, doing nothing.");
 }
 
 bool HuberoPlannerROS::setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan) {
@@ -56,7 +86,8 @@ bool HuberoPlannerROS::isGoalReached() {
 }
 
 void HuberoPlannerROS::reconfigureCB(HuberoPlannerConfig &config, uint32_t level) {
-	cfg_.reconfigure(config);
+	printf("HuberoPlannerROS::reconfigureCB \r\n");
+	cfg_->reconfigure(config);
 }
 
 void HuberoPlannerROS::publishLocalPlan(std::vector<geometry_msgs::PoseStamped>& path) {
---
+++ src/test_executable.cpp
@@ -0,0 +1,32 @@
+/*
+ * test_executable.cpp
+ *
+ *  Created on: Feb 24, 2021
+ *      Author: rayvburn
+ */
+
+//#include <hubero_local_planner/hubero_planner.h>
+//// TEST: HuberoPlanner dtor + LocalPlannerUtil shared_ptr
+//int main(int argc, char** argv) {
+//	ros::init(argc, argv, "hubero_local_planner_exe");
+//
+//	typedef base_local_planner::LocalPlannerUtil Util;
+//	std::shared_ptr<Util> util = std::make_shared<Util>();
+//	tf::TransformListener tf;
+//	costmap_2d::Costmap2D costmap;
+//	util->initialize(&tf, &costmap, "test1");
+//	hubero_local_planner::HuberoPlanner planner("test", util);
+//
+//	return 0;
+//}
+
+//#include <ros/ros.h>
+//#include <hubero_local_planner/fuzz/processor.h>
+//
+//int main(int argc, char** argv) {
+//	ros::init(argc, argv, "hubero_local_planner_exe");
+//
+//	fuzz::Processor proc;
+//
+//	return 0;
+//}
