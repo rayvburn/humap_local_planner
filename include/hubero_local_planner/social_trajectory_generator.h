ad4972eb283acfa5ab98d6f3e129b45a03533475
Jarek Karwowski <chromedivizer@gmail.com>
2022-03-23 12:23:10 +0100

    `SocialTrajectoryGenerator` - added initializer of trajectories w/o planning, added `generateTrajectoryWithoutPlanning`, added param to force computation methods (motion data update), added getters to motion data

--- include/hubero_local_planner/social_trajectory_generator.h
+++ include/hubero_local_planner/social_trajectory_generator.h
@@ -96,6 +96,19 @@ public:
 	);
 
 	/**
+	 * @brief Version of @ref initialise without @ref TrajectorySamplingParams
+	 * Useful for @ref generateTrajectoryWithoutPlanning
+	 */
+	void initialise(
+		const World& world_model,
+		const geometry::Vector& robot_local_vel,
+		std::shared_ptr<const base_local_planner::LocalPlannerLimits> limits_lp_ptr,
+		const double& robot_mass,
+		const double& twist_rotation_compensation,
+		bool discretize_by_time = false
+	);
+
+	/**
 	 * Whether this generator can create more trajectories
 	 *
 	 * @note Based on base_local_planner::SimpleTrajectoryGenerator::hasMoreTrajectories
@@ -123,6 +136,53 @@ public:
 	 * @brief Prints fuzzy inference system configuration
 	 */
 	void printFisConfiguration() const;
+
+	/**
+	 * @brief Calculates next trajectory to the goal without deep investigation into the future
+	 *
+	 * Calculations are performed according to a fixed set of parameters
+	 *
+	 * @param world_model model of robot and its environment
+	 * @param traj trajectory object that will be filled up according to calculations
+	 * @return true if traj complies with local planner's velocity limits
+	 * @return false if traj exceeds local planner's velocity limits
+	 */
+	bool generateTrajectoryWithoutPlanning(base_local_planner::Trajectory& traj);
+
+	/**
+	 * @brief Retrieves internal force vector computed during the last @ref computeForces with motion data update req.
+	 */
+	inline geometry::Vector getForceInternal() const {
+		return diag_force_internal_;
+	}
+
+	/**
+	 * @brief Retrieves interaction force vector computed during the last @ref computeForces with motion data update req.
+	 */
+	inline geometry::Vector getForceInteraction() const {
+		return diag_force_interaction_;
+	}
+
+	/**
+	 * @brief Retrieves social force vector computed during the last @ref computeForces with motion data update
+	 */
+	inline geometry::Vector getForceSocial() const {
+		return diag_force_social_;
+	}
+
+	/**
+	 * @brief Retrieves robot-obstacle poses computed during the last @ref computeForces with motion data update req.
+	 */
+	inline std::vector<geometry::Pose> getRobotObstacleDistances() const {
+		return diag_closest_points_;
+	}
+
+	/**
+	 * @brief Retrieves fuzzy behaviour computed during the last @ref computeForces with motion data update req.
+	 */
+	inline std::string getActiveFuzzyBehaviour() const {
+		return diag_behaviour_active_;
+	}
 
 	static std::vector<double> computeAmplifierSamples(double amplifier_min, double amplifier_max, double granularity);
 
@@ -156,13 +216,16 @@ protected:
 
 	/**
 	 * @brief Wraps all necessary force calculations required to compute a velocity sample
+	 *
+	 * @param update_motion_data should be set to true for first/single trajectory calculation
 	 */
 	void computeForces(
 		const World& world_model,
 		geometry::Vector& force_internal,
 		geometry::Vector& force_interaction_dynamic,
 		geometry::Vector& force_interaction_static,
-		geometry::Vector& force_human_action
+		geometry::Vector& force_human_action,
+		bool update_motion_data = false
 	);
 
 	/// Turns true when @ref setParameters was called
@@ -204,6 +267,17 @@ protected:
 	sfm::SocialForceModel sfm_;
 	fuzz::Processor fuzzy_processor_;
 	fuzz::SocialConductor social_conductor_;
+	/// @}
+
+	/**
+	 * @defgroup diagnostics Diagnostics data
+	 * @{
+	 */
+	geometry::Vector diag_force_internal_;
+	geometry::Vector diag_force_interaction_;
+	geometry::Vector diag_force_social_;
+	std::vector<geometry::Pose> diag_closest_points_;
+	std::string diag_behaviour_active_;
 	/// @}
 
 }; // class SocialTrajectoryGenerator
