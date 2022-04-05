295ef9cc7a0b6714b035894e71a734f1fe19bc2a
Jarek Karwowski <chromedivizer@gmail.com>
2022-04-05 15:12:33 +0200

    added `SpeedyGoalCostFunction` class

---
+++ include/hubero_local_planner/speedy_goal_cost_function.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <base_local_planner/trajectory_cost_function.h>
+#include <hubero_local_planner/geometry/pose.h>
+
+namespace hubero_local_planner {
+
+/**
+ * @brief Cost function that penalizes high speeds at close distances to global goal
+ */
+class SpeedyGoalCostFunction: public base_local_planner::TrajectoryCostFunction {
+public:
+	SpeedyGoalCostFunction(const geometry::Pose& global_goal);
+
+	void setParameters(const double& distance_slow_down, const double vel_trans_ideal);
+
+	/**
+	 * @brief General updating of context values if required.
+	 * Subclasses may overwrite. Return false in case there is any error.
+	 */
+	virtual bool prepare() override;
+
+	/**
+	 * @brief Returns a score for trajectory @ref traj
+	 */
+	virtual double scoreTrajectory(base_local_planner::Trajectory& traj) override;
+
+protected:
+	/// Pose of the global goal
+	const geometry::Pose& global_goal_;
+
+	double distance_slow_down_;
+	double vel_trans_ideal_;
+};
+
+} // namespace hubero_local_planner
