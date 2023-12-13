e0aaaa67eca9cb22afc554ea232a3fb7109f088b
Jarek Karwowski <chromedivizer@gmail.com>
2023-12-14 00:51:01 +0100

    added `RecoveryManager` class that detects situations that may require executing special recovery behaviours
    - somehow related to #58 and #68

---
+++ include/humap_local_planner/recovery_manager.h
@@ -0,0 +1,253 @@
+#pragma once
+
+#include <base_local_planner/map_grid_cost_function.h>
+#include <base_local_planner/trajectory.h>
+
+#include <humap_local_planner/obstacle_separation_cost_function.h>
+
+// custom implementation of the CircularBuffer
+#include <deque>
+#include <stdexcept>
+
+namespace humap_local_planner {
+
+template <typename T>
+class CircularBuffer {
+public:
+	explicit CircularBuffer(size_t capacity = 0):
+		buffer_(), capacity_(capacity), size_(0) {}
+
+	T& at(size_t index) {
+		return buffer_.at(index);
+	}
+
+	void push_back(const T& value) {
+		if (size_ == capacity_) {
+			// Remove the oldest element if the buffer is full
+			buffer_.pop_front();
+			size_--;
+		}
+		buffer_.push_back(value);
+		size_++;
+	}
+
+	T& front() {
+		if (empty()) {
+			throw std::out_of_range("Circular buffer is empty");
+		}
+		return buffer_.front();
+	}
+
+	void pop_front() {
+		if (empty()) {
+			throw std::out_of_range("Circular buffer is empty");
+		}
+		buffer_.pop_front();
+		size_--;
+	}
+
+	bool empty() const {
+		return size_ == 0;
+	}
+
+	size_t size() const {
+		return size_;
+	}
+
+	void set_capacity(size_t new_capacity) {
+		capacity_ = new_capacity;
+		while (size_ > capacity_) {
+			buffer_.pop_front();
+			size_--;
+		}
+	}
+
+	void clear() {
+		buffer_.clear();
+	}
+
+	size_t capacity() const {
+		return capacity_;
+	}
+
+	const std::deque<T>& get_deque() const {
+		return buffer_;
+	}
+
+protected:
+	std::deque<T> buffer_;
+	size_t capacity_;
+	size_t size_;
+};
+
+/**
+ * @brief
+ *
+ * This class is partially based on TEB's FailureDetector by Christoph Rösmann
+ */
+class RecoveryManager {
+public:
+	/// How many points around the robot should be checked as the recovery goal pose candidates
+	static constexpr int RECOVERY_ROTATE_AND_RECEDE_SURROUNDING_PTS = 16;
+	/// Defines length of the path whose poses must be collision-free to choose it for recovery
+	static constexpr double RECOVERY_ROTATE_AND_RECEDE_DISTANCE = 0.3;
+
+	RecoveryManager();
+
+	/**
+	 * @brief Set buffer length (measurement history) for oscillation detection
+	 * @param length number of measurements to be kept
+	 */
+	void setOscillationBufferLength(int length);
+
+	/**
+	 * @brief Adds a new twist measurement to the internal buffer and computes a new decision
+	 *
+	 * @param v_x linear X component of the current velocity command
+	 * @param v_th angular Z component of the current velocity command
+	 * @param v_max maximum forward translational velocity
+	 * @param v_backwards_max maximum backward translational velocity
+	 * @param omega_max maximum angular velocity
+	 * @param v_eps Threshold for the average normalized linear velocity in (0,1) that must not be exceeded (e.g. 0.1)
+	 * @param omega_eps Threshold for the average normalized angular velocity in (0,1) that must not be exceeded (e.g. 0.1)
+	 */
+	void checkOscillation(
+		double v_x,
+		double v_th,
+		double v_max,
+		double v_backwards_max,
+		double omega_max,
+		double v_eps,
+		double omega_eps
+	);
+
+	/**
+	 * @brief Checks whether the robot's trajectory deviates from the gradient-based cost functions
+	 *
+	 * Deviation may occur, e.g., when obstacles suddenly appear in front of the robot of nearby
+	 *
+	 * @param traj robot trajectory to be evaluated
+	 * @param alignment_costfun non-const only because scoreTrajectory method is non-const
+	 * @param goal_front_costfun non-const only because scoreTrajectory method is non-const
+	 */
+	void checkDeviation(
+		base_local_planner::Trajectory traj,
+		base_local_planner::MapGridCostFunction& goal_front_costfun,
+		base_local_planner::MapGridCostFunction& alignment_costfun
+	);
+
+	/**
+	 * @brief Checks whether the robot's footprint is in collision with the environment according to the costmap
+	 *
+	 * @param traj robot trajectory to be evaluated
+	 * @param obstacle_costfun non-const only because scoreTrajectory method is non-const
+	 */
+	void checkCollision(
+		base_local_planner::Trajectory traj,
+		ObstacleSeparationCostFunction& obstacle_costfun
+	);
+
+	/**
+	 * @brief Searches for a safe pose near the robot to recover from being stuck
+	 *
+	 * @param x x-component of the current robot pose
+	 * @param y y-component of the current robot pose
+	 * @param th theta-component of the current robot pose
+	 * @param obstacle_costfun cost function that evaluates whether a pose in world coordinates is collision-free
+	 * @return true If recovery goal pose is found (recovery is feasible)
+	 * @return false If recovery goal pose was not found
+	 */
+	bool planRecoveryRotateAndRecede(
+		double x,
+		double y,
+		double th,
+		const ObstacleSeparationCostFunction& obstacle_costfun
+	);
+
+	/**
+	 * @brief Clear the current internal state
+	 *
+	 * This call also resets the internal buffer
+	 */
+	void clear();
+
+	/**
+	 * @brief Check if the robot is oscillating (and possibly stuck totally soon)
+	 */
+	inline bool isOscillating() const {
+		return oscillating_;
+	}
+
+	inline bool isDeviatingFromGlobalPath() const {
+		return deviating_from_global_plan_;
+	}
+
+	inline bool isStuck() const {
+		return stuck_;
+	}
+
+	inline bool isNearCollision() const {
+		return near_collision_;
+	}
+
+	inline bool canRecover() const {
+		return can_recover_from_collision_;
+	}
+
+	inline double getRecoveryGoalX() const {
+		return recovery_goal_x_;
+	}
+
+	inline double getRecoveryGoalY() const {
+		return recovery_goal_y_;
+	}
+
+	inline double getRecoveryGoalYaw() const {
+		return recovery_goal_th_;
+	}
+
+	static bool isPoseCollisionFree(double costmap_cost);
+
+protected:
+	/** Variables to be monitored */
+	struct VelMeasurement {
+		double v = 0;
+		double omega = 0;
+	};
+
+	/**
+	 * @brief Detect if the robot got stucked based on the current buffer content
+	 *
+	 * @param v_eps Threshold for the average normalized linear velocity in (0,1) that must not be exceded (e.g. 0.1)
+	 * @param omega_eps Threshold for the average normalized angular velocity in (0,1) that must not be exceded (e.g. 0.1)
+	 */
+	void detectOscillation(double v_eps, double omega_eps);
+
+	/// Sets all coordinates of the recovery goal to NAN
+	void resetRecoveryGoal();
+
+	/// Circular buffer to store the last measurements @see setOscillationBufferLength
+	CircularBuffer<VelMeasurement> buffer_;
+
+	/// Current state: true if robot is oscillating
+	bool oscillating_;
+
+	/// True if the robot is stuck (based on the data in the buffer)
+	bool stuck_;
+
+	bool deviating_from_global_plan_;
+
+	bool near_collision_;
+	bool can_recover_from_collision_;
+
+	/// Coordinates of the recovery goal
+	double recovery_goal_x_;
+	double recovery_goal_y_;
+	double recovery_goal_th_;
+
+	/// Stores previous linear velocity of the velocity command send to the mobile base
+	double prev_cmd_vx;
+	double prev_cmd_omega;
+};
+
+} // namespace humap_local_planner
---
+++ src/recovery_manager.cpp
@@ -0,0 +1,286 @@
+#include <humap_local_planner/recovery_manager.h>
+
+#include <costmap_2d/cost_values.h>
+#include <humap_local_planner/geometry/pose.h>
+
+namespace humap_local_planner {
+
+RecoveryManager::RecoveryManager():
+	buffer_(10),
+	oscillating_(false),
+	stuck_(false),
+	deviating_from_global_plan_(false),
+	near_collision_(false),
+	can_recover_from_collision_(true),
+	recovery_goal_x_(NAN),
+	recovery_goal_y_(NAN),
+	recovery_goal_th_(NAN),
+	prev_cmd_vx(0.0),
+	prev_cmd_omega(0.0)
+{}
+
+void RecoveryManager::setOscillationBufferLength(int length) {
+	buffer_.set_capacity(length);
+}
+
+void RecoveryManager::checkOscillation(
+	double v_x,
+	double v_th,
+	double v_max,
+	double v_backwards_max,
+	double omega_max,
+	double v_eps,
+	double omega_eps
+) {
+	if (buffer_.capacity() == 0) {
+		return;
+	}
+
+	RecoveryManager::VelMeasurement measurement;
+	// just consider linear velocity in x-direction in the robot frame for now
+	measurement.v = prev_cmd_vx;
+	measurement.omega = prev_cmd_omega;
+
+	// buffer stores normalized velocities
+	if (measurement.v > 0 && v_max > 0) {
+		measurement.v /= v_max;
+	} else if (measurement.v < 0 && v_backwards_max > 0) {
+		measurement.v /= v_backwards_max;
+	}
+
+	if (omega_max > 0) {
+		measurement.omega /= omega_max;
+	}
+	buffer_.push_back(measurement);
+
+	// immediately compute new state
+	detectOscillation(v_eps, omega_eps);
+
+	prev_cmd_vx = v_x;
+	prev_cmd_omega = v_th;
+}
+
+void RecoveryManager::checkDeviation(
+	base_local_planner::Trajectory traj,
+	base_local_planner::MapGridCostFunction& goal_front_costfun,
+	base_local_planner::MapGridCostFunction& alignment_costfun
+) {
+	double cost_alignment = alignment_costfun.scoreTrajectory(traj);
+	double goal_front_cost = goal_front_costfun.scoreTrajectory(traj);
+
+	if (
+		cost_alignment == alignment_costfun.obstacleCosts()
+		|| cost_alignment == alignment_costfun.unreachableCellCosts()
+		|| goal_front_cost == goal_front_costfun.obstacleCosts()
+		|| goal_front_cost == goal_front_costfun.unreachableCellCosts()
+	) {
+		deviating_from_global_plan_ = true;
+		return;
+	}
+	deviating_from_global_plan_ = false;
+}
+
+void RecoveryManager::checkCollision(
+	base_local_planner::Trajectory traj,
+	ObstacleSeparationCostFunction& obstacle_costfun
+) {
+	double occdist_cost = obstacle_costfun.scoreTrajectory(traj);
+	if (RecoveryManager::isPoseCollisionFree(occdist_cost)) {
+		// we're safe
+		near_collision_ = false;
+		can_recover_from_collision_ = true;
+		resetRecoveryGoal();
+		return;
+	}
+	if (occdist_cost >= costmap_2d::LETHAL_OBSTACLE) {
+		// something went wrong
+		near_collision_ = true;
+		can_recover_from_collision_ = false;
+		return;
+	}
+	if (occdist_cost < 0.0) {
+		// footprint seems to be in collision with some object marked on the costmap (may be outdated)
+		near_collision_ = true;
+		// we can attempt to recover from a collision once the separation distance is positive (there should still
+		// be some gap between the obstacle and the robot)
+		double obstacle_keepout_distance = obstacle_costfun.getSeparationDistance();
+		can_recover_from_collision_ = obstacle_keepout_distance > 0.0;
+	}
+}
+
+bool RecoveryManager::planRecoveryRotateAndRecede(
+	double x,
+	double y,
+	double th,
+	const ObstacleSeparationCostFunction& obstacle_costfun
+) {
+	// first, check whether it is possible to stick to the previous recovery pose
+	bool recovery_goal_valid =
+		!std::isinf(recovery_goal_x_)
+		&& !std::isinf(recovery_goal_y_)
+		&& !std::isinf(recovery_goal_th_);
+	if (recovery_goal_valid) {
+		auto rgoal_cost = obstacle_costfun.getFootprintCost(recovery_goal_x_, recovery_goal_y_, recovery_goal_th_);
+		if (RecoveryManager::isPoseCollisionFree(rgoal_cost)) {
+			// we'll use the previously found goal pose for the recovery routine
+			return true;
+		}
+	}
+
+	// check the cost at the current position but without separation distance
+	double pos_cost_wo_separation = obstacle_costfun.getFootprintCost(x, y, th, 0.0);
+	if (pos_cost_wo_separation == costmap_2d::LETHAL_OBSTACLE || pos_cost_wo_separation < 0.0) {
+		resetRecoveryGoal();
+		return false;
+	}
+
+	// Given the current pose, let's try to find a point somewhere around the mobile base, so the robot could align
+	// towards it (adjust orientation in place) and slowly escape from a stuck pose.
+	// Prepare a vector of recovery directions - aim is to check the "front ones" first
+	auto generateMultipliersSequence = [](size_t length) -> std::vector<int> {
+		int start = 0;
+		int end = length - 1;
+		std::vector<int> container;
+		for (int i = 0; i <= (length / 2); i++) {
+			if (start <= end) {
+				container.push_back(start++);
+			}
+			if (start <= end) {
+				container.push_back(end--);
+			}
+		}
+		return container;
+	};
+	// stores a sequence of multipliers in a following order (for 8 pts), e.g., 0,1,8,2,7,3,6,4,5
+	auto angle_multipliers = generateMultipliersSequence(RECOVERY_ROTATE_AND_RECEDE_SURROUNDING_PTS);
+
+	// vector of directions ordered according to the sequence above - the "front ones" first;
+	// "front ones" are expressed according to the robot's local coordinate system
+	std::vector<double> checked_directions;
+	double th_delta = (M_PI + M_PI) / static_cast<double>(RECOVERY_ROTATE_AND_RECEDE_SURROUNDING_PTS);
+	for (const auto& mult: angle_multipliers) {
+		geometry::Angle angle(th + static_cast<double>(mult) * th_delta);
+		checked_directions.push_back(angle.getRadian());
+	}
+
+	// helper function that finds coordinates of the shifted pose compared to the reference one
+	auto computeCoordsAtDistanceAndDirection = [](
+		double x,
+		double y,
+		double th,
+		double direction,
+		double distance,
+		double waypoint_separation = 0.025
+	) -> std::vector<geometry::Vector> {
+		geometry::Angle dir_angle(direction);
+		geometry::Vector vec_towards_target(dir_angle);
+		vec_towards_target *= waypoint_separation;
+		size_t num_of_poses = std::ceil(distance / waypoint_separation);
+
+		std::vector<geometry::Vector> container;
+		container.emplace_back(x, y, th);
+		for (size_t i = 0; i < num_of_poses; i++) {
+			double x_shifted = container.back().getX() + vec_towards_target.getX();
+			double y_shifted = container.back().getY() + vec_towards_target.getY();
+			double th_shifted = geometry::Angle(direction).getRadian();
+			container.emplace_back(x_shifted, y_shifted, th_shifted);
+		}
+		return container;
+	};
+
+	// let's assume that we'll try to travel N cm plus the separation distance
+	double dist = RECOVERY_ROTATE_AND_RECEDE_DISTANCE + obstacle_costfun.getSeparationDistance();
+
+	// iterate over the investigated directions - choose the first one (the most upfront)
+	for (int i = 0; i < RECOVERY_ROTATE_AND_RECEDE_SURROUNDING_PTS; i++) {
+		// find a pose to evaluate against the cost function - search among the straight paths
+		auto poses_eval = computeCoordsAtDistanceAndDirection(
+			x,
+			y,
+			th,
+			checked_directions.at(i),
+			dist
+		);
+		// flag indicating that any intermediate pose is in collision
+		bool in_collision = false;
+		// iterate over the poses of the straight path
+		for (const auto& pose: poses_eval) {
+			auto cost = obstacle_costfun.getFootprintCost(pose.getX(), pose.getY(), pose.getZ(), 0.0);
+			if (cost < 0.0 || cost >= costmap_2d::LETHAL_OBSTACLE) {
+				in_collision = true;
+				break;
+			}
+		}
+		if (in_collision) {
+			continue;;
+		}
+		// found an obstacle-free pose lying nearby along the straight path
+		recovery_goal_x_ = poses_eval.back().getX();
+		recovery_goal_y_ = poses_eval.back().getY();
+		recovery_goal_th_ = poses_eval.back().getZ();
+		return true;
+	}
+
+	// not found a valid recovery pose
+	resetRecoveryGoal();
+	return false;
+}
+
+void RecoveryManager::clear() {
+	buffer_.clear();
+	oscillating_ = false;
+}
+
+bool RecoveryManager::isPoseCollisionFree(double costmap_cost) {
+	return costmap_cost >= 0.0 && costmap_cost < costmap_2d::LETHAL_OBSTACLE;
+}
+
+void RecoveryManager::detectOscillation(double v_eps, double omega_eps) {
+	oscillating_ = false;
+
+	// // we start detecting only as soon as we have the buffer filled at least half
+	if (buffer_.size() < (buffer_.capacity() / 2)) {
+		return;
+	}
+
+	double n = (double)buffer_.size();
+
+	// helper
+	auto sign = [](double x) -> int {
+		return (x < 0.0) ? -1 : +1;
+	};
+
+	// compute mean for v and omega
+	double v_mean = 0;
+	double omega_mean = 0;
+	int omega_zero_crossings = 0;
+	for (int i = 0; i < n; ++i) {
+		v_mean += buffer_.at(i).v;
+		omega_mean += buffer_.at(i).omega;
+		if (i == 0) {
+			continue;
+		}
+		if (sign(buffer_.at(i).omega) != sign(buffer_.at(i-1).omega)) {
+			++omega_zero_crossings;
+		}
+	}
+	v_mean /= n;
+	omega_mean /= n;
+
+	// oscillated
+	if (omega_zero_crossings > 1 && std::abs(v_mean) < v_eps && std::abs(omega_mean) < omega_eps) {
+		oscillating_ = true;
+	}
+	// stuck completely
+	if (omega_zero_crossings == 0 && std::abs(v_mean) < 1e-03 && std::abs(omega_mean) < 1e-03) {
+		stuck_ = true;
+	}
+}
+
+void RecoveryManager::resetRecoveryGoal() {
+	recovery_goal_x_ = NAN;
+	recovery_goal_y_ = NAN;
+	recovery_goal_th_ = NAN;
+}
+
+} // namespace humap_local_planner
