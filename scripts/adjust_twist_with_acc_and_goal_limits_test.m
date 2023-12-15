9e3dd5a75b535f8973ba30d1d4ef1db500c0c4e9
Jarek Karwowski <chromedivizer@gmail.com>
2023-12-16 00:25:58 +0100

    `transformations` - improved the `adjustTwistWithAccAndGoalLimits` method that computes max. velocity according to accel. limits and distance to goal
    - Matlab script updated too

--- scripts/adjust_twist_with_acc_and_goal_limits_test.m
+++ scripts/adjust_twist_with_acc_and_goal_limits_test.m
@@ -27,21 +27,26 @@ VEL_CMD = [0.50; 0.0; 0.0];
 vel_local = VEL_LOCAL_INIT;
 robot_pose = ROBOT_INIT_POSE;
 
+% helps to detect reaching the goal pose (to finish the simulation)
+prev_dist_to_goal = Inf;
+
 figure;
 for i=1:1e06
     dist_to_goal = norm(GOAL_POSE(1:2) - robot_pose(1:2));
-    if dist_to_goal <= 0.05
+    if dist_to_goal <= 0.0001 || prev_dist_to_goal < dist_to_goal
+        fprintf("Successfully finished the simulation\n")
         break;
     end
+    prev_dist_to_goal = dist_to_goal;
     
     [~, vel_out] = adjustTwistWithAccAndGoalLimits(vel_local, LIMITS, VEL_CMD, MAINTAIN_VEL_COMPONENTS_RATE, dist_to_goal, SIM_TIME);
     vel_local = vel_out;
     
     plot(dist_to_goal, vel_local(1), 'ro')
-    %plot(i, vel_local(1), 'ro')
+    %plot(i * SIM_PERIOD, vel_local(1), 'ro')
     hold on;
     plot(dist_to_goal, vel_local(2), 'go')
-    %plot(i, vel_local(2), 'go')
+    %plot(i * SIM_PERIOD, vel_local(2), 'go')
     
     robot_pose = computeNextPoseBaseVel(robot_pose, vel_local, SIM_PERIOD);
 end
