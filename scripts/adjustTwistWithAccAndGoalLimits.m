fcbf2a5ff182dbb8c3012b0f5a67a0f406e83b9c
Jarek Karwowski <chromedivizer@gmail.com>
2023-12-16 00:25:58 +0100

    `transformations` - improved the `adjustTwistWithAccAndGoalLimits` method that computes max. velocity according to accel. limits and distance to goal
    - Matlab script updated too

--- scripts/adjustTwistWithAccAndGoalLimits.m
+++ scripts/adjustTwistWithAccAndGoalLimits.m
@@ -38,9 +38,24 @@ function [status, cmd_vel] = adjustTwistWithAccAndGoalLimits( ...
         % vel_max_x_corrected = max([min([vel_max_x, dist_to_goal / sim_time]), vel_min_x])
         % vel_max_y_corrected = max([min([vel_max_y, dist_to_goal / sim_time]), vel_min_y])
         %
-        % improved version allows going faster for longer
-        max_vx = 2 * dist_to_goal / acc_lim_x;
-        max_vy = 2 * dist_to_goal / acc_lim_y;
+        % second version which often starts limiting the velocity commands too late and thus violates accel. limits
+        % when decelerating
+        % max_vx = 2 * dist_to_goal / acc_lim_x;
+        % max_vy = 2 * dist_to_goal / acc_lim_y;
+        % vel_max_x_corrected = max([min([vel_max_x, max_vx]), vel_min_x]);
+        % vel_max_y_corrected = max([min([vel_max_y, max_vy]), vel_min_y]);
+        %
+        % third version
+        acc_lim_decel = hypot(acc_lim_x, acc_lim_y);
+        % maximum initial speed allowing to stop after dist_to_goal given a deceleration rate of "acc_lim_decel"
+        speed_init_max = sqrt(2.0 * acc_lim_decel * dist_to_goal);
+
+        % to trim both velocity components proportionally
+        vel_vector_angle = atan2(vel_curr(2), vel_curr(1));
+        % updated velocity limits that prevent from overshooting the goal pose
+        max_vx = cos(vel_vector_angle) * speed_init_max;
+        max_vy = sin(vel_vector_angle) * speed_init_max;
+        % new limits can only be smaller than the original ones
         vel_max_x_corrected = max([min([vel_max_x, max_vx]), vel_min_x]);
         vel_max_y_corrected = max([min([vel_max_y, max_vy]), vel_min_y]);
     end
